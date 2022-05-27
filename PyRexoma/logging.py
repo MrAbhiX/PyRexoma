1e1882e1a16d89d372cf84d89892cfc138bc25c0
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-27 19:26:08 +0530

    Create logging.py

---
+++ PyRexoma/logging.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
+
+Rexoma = logging.getLogger(__name__)
